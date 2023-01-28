1b5ecf8066d22c2554d84a3577b3ac4e71944ddc
Micaela Pañoni <micapagnoni97@gmail.com>
2023-01-28 10:42:39 -0300

    Logré activar los eventos de Javascript

--- DESARROLLO WEB/Javascript/JS/function.js
+++ DESARROLLO WEB/Javascript/JS/function.js
@@ -7,3 +7,5 @@ let nro2 = 4;
 let resultado = nro1+nro2;
 
 console.log("La suma de nro1 y nro2 es:"+resultado)
+
+//onclick='document.getElementById("demo").innerHTML//
---
+++ DESARROLLO WEB/Javascript/JS/function2.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function() {
+    console.log ("Capturamos el evento CLICK");
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
+}
