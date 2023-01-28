b2c5bf8de8e1b32a228daaf6930fed3d53c00206
Micaela Pañoni <micapagnoni97@gmail.com>
2023-01-28 19:47:45 -0300

    Logré crear una mini calculadora usando eventos de Javascript

--- DESARROLLO WEB/Javascript/JS/function2.js
+++ DESARROLLO WEB/Javascript/JS/function2.js
@@ -1,4 +1,28 @@
-document.getElementById("boton").onclick = function() {
+/**  document.getElementById("boton").onclick = function() {
     console.log ("Capturamos el evento CLICK");
     document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
+} 
+
+ document.addEventListener("click", function () { 
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML= "Estamos probando nuestro primer evento en JS";
+}); */
+
+document.getElementById("boton").addEventListener("click", function () { 
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML= "Estoy probando mis primeros eventos en JS";
+}) ; 
+
+document.getElementById("boton_color").addEventListener("click", function () {
+    document.body.style.backgroundColor= "#C70039";
+});
+
+document.getElementById("boton_ocultar").addEventListener("click", function () {
+    document.getElementById("demo").style.display = "none";
+});
+
+const collection = document.getElementsByClassName("prueba");
+for (let i= 0; i < collection.length; i++) {
+    collection[i].style.backgroundColor = "green";
+    
 }
